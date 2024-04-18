218671531be75d3836bbb8b6e567759a68b02ee8
Mohamed Emara <medoemara104@gmail.com>
2024-04-18 19:25:17 +0200

    Implement login users

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { LoginDTO } from './dto/login.dto';
+import { User } from 'src/users/users.entity';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UsersService) {}
+
+  async login(loginDTO: LoginDTO): Promise<User> {
+    // 1- Get user by email
+    const user = await this.userService.findOne(loginDTO);
+
+    // 2- Copmpare passwords
+    const mathcedPassword = await bcrypt.compare(
+      loginDTO.password,
+      user.password,
+    );
+    if (mathcedPassword) {
+      delete user.password;
+      return user;
+    } else {
+      throw new UnauthorizedException('Wrong email or password');
+    }
+  }
+}
